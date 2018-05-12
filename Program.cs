8d409f1007b73e484ac8b2e7c6760b209e360e3d
IsaevR <diplomatic.area@gmail.com>
2018-05-12 08:54:56 +0300

    firstcommit

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace CeasarEncription
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите слово,которое нужно зашифровать:");
+            string s = Console.ReadLine();
+
+            string abc = "АБВГДЕЖЗИКЛМНОПРСТУФХЦЧШЩЬЪЭЮЯ";
+
+            int key = 3;
+
+            List<string> result = new List<string>();
+            
+            
+            result.Add(Ecription(s.ToUpper(), abc, key));
+            
+
+            foreach (var word in result)
+            {
+                Console.WriteLine("Зашифрованное слово:" + word.ToLower());
+            }
+
+            Console.ReadLine();
+
+
+        }
+
+
+        // метод которы шифрует строку способом сдвига каждого символа в право на три шага
+        // он же называется Шифрованием Цезаря.
+        static string Ecription(string targetWord, string abc, int key)
+        {
+            string result = string.Empty;
+            for (int i = 0; i < targetWord.Length; i++)
+            {
+                for (int j = 0; j < abc.Length; j++)
+                {
+                    if (targetWord[i] == abc[j])
+                    {
+                        int temp = j + key;
+
+                        while (temp >= abc.Length)
+                            temp -= abc.Length;
+                        result += abc[temp];
+                    }
+                }
+            }
+            return result;
+        }
+    }
+    
+}
